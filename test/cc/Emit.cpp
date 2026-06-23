1a1e8172bcb6630de9faf9dd9f33176d4e043975
nella <simontupy64@gmail.com>
2026-06-23 13:22:52 +0200

    Emit IR WIP porting.

--- test/cc/Emit.cpp
+++ test/cc/Emit.cpp
@@ -491,12 +491,371 @@ namespace rat::cc {
 			// i.e. (1 + 0i), to/from it, affecting only the real part.
 			CType ct = completeComplex(type);
 			Value oldVal = {lv.addr, ct};
-			return nullptr;
+			Node* re = complexReal(fn, oldVal);
+			Node* im = complexImag(fn, oldVal);
+			Node* one = fn.constFloat(irType(complexElem(ct)), 1.0);
+			Node* newRe = fn.binary(isInc ? Opcode::FAdd : Opcode::FSub, re, one);
+			Value updated = makeComplex(fn, ct, newRe, im);
+			storeComplex(fn, lv.addr, ct, updated);
+			return isPre ? Value{lv.addr, ct} : makeComplex(fn, ct, re, im);
+		}
+		Node* old = loadLValue(fn, lv);
+		Node* updated;
+		if (isFloating(type)) {
+			Node* delta = fn.constFloat(irType(type), 1.0);
+			updated = fn.binary(isInc ? Opcode::FAdd : Opcode::FSub, old, delta);
+		} else {
+			Node* delta = isPointer(type) ? constSize(fn, byteSize(pointee(type)))
+																		: fn.constInt(irType(type), 1);
+			updated = isInc ? fn.add(old, delta) : fn.sub(old, delta);
+		}
+		storeLValue(fn, lv, updated);
+		return {isPre ? updated : old, type};
+	}
 
-		Node* result = nullptr;
-		switch (e->binary.op) {
-		case ExprOp::Assign:
-			result = rhs;
+	Emitter::Value Emitter::emitStructAssign(Function& fn, const Expr* e,
+																					 const LValue& lv, Value rhs) {
+		CType targetType = lv.type;
+		if (e->binary.op != ExprOp::Assign) {
+			fail("invalid compound assignment to a struct or union");
+			return {};
+		}
+		if (!isStruct(rhs.type) || rhs.type.strukt != targetType.strukt) {
+			fail("assigning to '" + typeName(targetType) + "' from incompatible '" +
+					 typeName(rhs.type) + "'");
+			return {};
+		}
+		emitMemCopy(fn, lv.addr, rhs.node, targetType.strukt->size);
+		return {lv.addr, targetType};
+	}
+
+	Emitter::Value Emitter::emitComplexAssign(Function& fn, const Expr* e,
+																						const LValue& lv, Value rhs) {
+		CType ct = completeComplex(lv.type);
+		Value stored;
+		if (e->binary.op == ExprOp::Assign) {
+			stored = {rhs.node, rhs.type};
+		} else {
+			ExprOp base;
+			B32 ok = compoundBaseOp(e->binary.op, base) &&
+							 (base == ExprOp::Add || base == ExprOp::Sub ||
+								base == ExprOp::Mul || base == ExprOp::Div);
+			if (!ok) {
+				fail("invalid compound assignment to a complex operand");
+				return {};
+			}
+			CType opType = completeComplex(usualArithmetic(ct, rhs.type));
+			Value cur = toComplex(fn, {lv.addr, ct}, opType);
+			Value rc = toComplex(fn, rhs, opType);
+			stored = emitComplexBinary(fn, base, cur, rc, opType);
+			if (!stored.node)
+				return {};
+		}
+		storeComplex(fn, lv.addr, ct, stored);
+		return {lv.addr, ct};
+	}
+
+	Emitter::Value Emitter::emitAssign(Function& fn, const Expr* e) {
+		LValue lv;
+		if (!emitLValue(fn, e->binary.lhs, lv))
+			return {};
+		if (lv.isArray) {
+			fail("array is not assignable");
+			return {};
+		}
+		if (isTopConst(lv.type)) {
+			fail("cannot assign to a const-qualified lvalue");
+			return {};
+		}
+		CType targetType = lv.type;
+		Value rhs = emitExpr(fn, e->binary.rhs);
+		if (!rhs.node)
+			return {};
+
+		if (isStruct(targetType))
+			return emitStructAssign(fn, e, lv, rhs);
+
+		if (isComplexType(targetType))
+			return emitComplexAssign(fn, e, lv, rhs);
+
+		Node* stored = nullptr;
+		if (e->binary.op == ExprOp::Assign) {
+			stored = convert(fn, rhs.node, rhs.type, targetType);
+		} else if (isPointer(targetType) && (e->binary.op == ExprOp::AddAssign ||
+																				 e->binary.op == ExprOp::SubAssign)) {
+			ExprOp op = e->binary.op == ExprOp::AddAssign ? ExprOp::Add : ExprOp::Sub;
+			Value res = emitPtrArith(fn, op, {loadLValue(fn, lv), targetType}, rhs);
+			if (!res.node)
+				return {};
+			stored = res.node;
+		} else {
+			ExprOp base;
+			if (!compoundBaseOp(e->binary.op, base)) {
+				fail("unsupported assignment operator");
+				return {};
+			}
+			CType ct = usualArithmetic(targetType, rhs.type);
+			Node* l = convert(fn, loadLValue(fn, lv), targetType, ct);
+			Node* r = convert(fn, rhs.node, rhs.type, ct);
+			Node* res = emitArith(fn, base, l, r, ct);
+			if (!res)
+				return {};
+			stored = convert(fn, res, ct, targetType);
+		}
+		storeLValue(fn, lv, stored);
+		return {stored, targetType};
+	}
+
+	B32 Emitter::emitBuiltinCall(Function& fn, const Expr* e, Value& out) {
+		if (!e->call.callee)
+			return false;
+		const String& b = *e->call.callee;
+
+		if (b == "__builtin_va_start" || b == "__builtin_va_end") {
+			List<Node*> args;
+			for (const Expr* arg : e->args) {
+				Value a = emitExpr(fn, arg);
+				if (!a.node)
+					return true;
+				args.push_back(a.node);
+			}
+			fn.call(b, nullptr, args);
+			CType v;
+			v.isVoid = true;
+			out = {fn.constInt(i32, 0), v};
+			return true;
+		}
+
+		if (b == "__builtin_expect") {
+			if (e->args.size() != 2) {
+				fail("__builtin_expect expects two arguments");
+				return true;
+			}
+			Value exp = emitExpr(fn, e->args[0]);
+			if (!exp.node)
+				return true;
+			Value hint = emitExpr(fn, e->args[1]);
+			if (!hint.node)
+				return true;
+			CType lng;
+			lng.bits = 64;
+			out = {convert(fn, exp.node, exp.type, lng), lng};
+			return true;
+		}
+		if (b == "__builtin_constant_p") {
+			if (e->args.size() != 1) {
+				fail("__builtin_constant_p expects one argument");
+				return true;
+			}
+			I64 v = 0;
+			B32 isConst = evalConst(e->args[0], v);
+			out = {fn.constInt(i32, isConst ? 1 : 0), ctInt()};
+			return true;
+		}
+		if (b == "__builtin_unreachable") {
+			CType vd;
+			vd.isVoid = true;
+			out = {fn.constInt(i32, 0), vd};
+			return true;
+		}
+		return false;
+	}
+
+	B32 Emitter::resolveCallee(Function& fn, const Expr* e, Callee& c) {
+		if (e->call.callee) {
+			auto found = funcs.find(*e->call.callee);
+			if (found != funcs.end()) {
+				c.direct = true;
+				c.sig = found->second;
+				c.prototyped = true;
+				return true;
+			}
+			Node* val = nullptr;
+			CType ct;
+			B32 isObject = false;
+			Local loc;
+			if (lookup(*e->call.callee, loc) && !loc.isArray) {
+				val = loc.inMem ? fn.load(irType(loc.type), loc.addr) : fn.get(loc.var);
+				ct = loc.type;
+				isObject = true;
+			} else {
+				auto g = globals.find(*e->call.callee);
+				if (g != globals.end()) {
+					val = fn.load(irType(g->second), fn.global(*e->call.callee));
+					ct = g->second;
+					isObject = true;
+				}
+			}
+			if (val && isFuncPtr(ct)) {
+				c.target = val;
+				c.ft = ct.func;
+				c.prototyped = true;
+			} else if (isObject) {
+				fail("called object is not a function or function pointer");
+				return false;
+			} else {
+				c.direct = true;
+				c.sig.ret = ctInt();
+			}
+			return true;
+		}
+		Value fpv = emitExpr(fn, e->call.target);
+		if (!fpv.node)
+			return false;
+		if (!isFuncPtr(fpv.type)) {
+			fail("called object is not a function");
+			return false;
+		}
+		c.target = fpv.node;
+		c.ft = fpv.type.func;
+		c.prototyped = true;
+		return true;
+	}
+
+	B32 Emitter::emitCallArgs(Function& fn, const Expr* e, const Callee& c,
+														U32 nparams, List<Node*>& args) {
+		for (U32 i = 0; i < e->args.size(); ++i) {
+			Value a = emitExpr(fn, e->args[i]);
+			if (!a.node)
+				return false;
+			CType pt;
+			if (i < nparams) {
+				pt = c.direct ? c.sig.params[i] : c.ft->params[i];
+			} else {
+				pt = defaultArgPromote(a.type);
+				if (isComplexType(pt)) {
+					pt.strukt = nullptr;
+					pt = completeComplex(pt);
+				}
+			}
+			if (isAggregate(pt)) {
+				Node* src = isComplexType(pt) ? toComplex(fn, a, pt).node : a.node;
+				Node* tmp = allocBytes(fn, pt.strukt->size);
+				emitMemCopy(fn, tmp, src, pt.strukt->size);
+				args.push_back(tmp);
+			} else {
+				args.push_back(convert(fn, a.node, a.type, pt));
+			}
+		}
+		return true;
+	}
+
+	Emitter::Value Emitter::emitCall(Function& fn, const Expr* e) {
+		Value builtin;
+		if (emitBuiltinCall(fn, e, builtin))
+			return builtin;
+
+		Callee c;
+		if (!resolveCallee(fn, e, c))
+			return {};
+
+		CType ret = c.direct ? c.sig.ret : c.ft->ret;
+		U32 nparams =
+				c.direct ? (U32)c.sig.params.size() : (U32)c.ft->params.size();
+		B32 unproto = c.direct ? c.sig.unprototyped : (c.ft && c.ft->unprototyped);
+		if (c.prototyped && !unproto) {
+			B32 variadic = c.direct ? c.sig.isVarArgs : c.ft->isVarArgs;
+			U32 nargs = (U32)e->args.size();
+			if (nargs < nparams || (!variadic && nargs > nparams)) {
+				fail(variadic ? "too few arguments to function call"
+											: "argument count does not match prototype");
+				return {};
+			}
+		}
+		List<Node*> args;
+		Node* resultSlot = nullptr;
+		if (isAggregate(ret)) {
+			resultSlot = allocBytes(fn, ret.strukt->size);
+			args.push_back(resultSlot);
+		}
+		if (!emitCallArgs(fn, e, c, nparams, args))
+			return {};
+		if (resultSlot) {
+			if (c.direct)
+				fn.call(*e->call.callee, mod.getPtr(), args);
+			else
+				fn.callIndirect(c.target, mod.getPtr(), args);
+			return {resultSlot, ret};
+		}
+		if (c.direct) {
+			if (isVoidType(c.sig.ret)) {
+				fn.call(*e->call.callee, nullptr, args);
+				return {fn.constInt(i32, 0), c.sig.ret};
+			}
+			return {fn.call(*e->call.callee, irType(c.sig.ret), args), c.sig.ret};
+		}
+		if (isVoidType(ret)) {
+			fn.callIndirect(c.target, nullptr, args);
+			return {fn.constInt(i32, 0), ret};
+		}
+		return {fn.callIndirect(c.target, irType(ret), args), ret};
+	}
+
+	Emitter::Value Emitter::emitAddrOf(Function& fn, const Expr* e) {
+		if (e->unary.operand->kind == ExprKind::Ident) {
+			auto f = funcs.find(*e->unary.operand->ident.name);
+			if (f != funcs.end())
+				return {fn.global(*e->unary.operand->ident.name),
+								funcPtrType(f->second)};
+			Local loc;
+			if (lookup(*e->unary.operand->ident.name, loc) && loc.isArray)
+				return {loc.addr, pointerTo(loc.type)};
+			if (globalArrays.count(*e->unary.operand->ident.name))
+				return emitExpr(fn, e->unary.operand);
+		}
+		LValue lv;
+		if (!emitLValue(fn, e->unary.operand, lv))
+			return {};
+		if (lv.isVar) {
+			fail("cannot take the address of an SSA value");
+			return {};
+		}
+		return {lv.addr, pointerTo(lv.type)};
+	}
+
+	Emitter::Value Emitter::emitDeref(Function& fn, const Expr* e) {
+		Value p = emitExpr(fn, e->unary.operand);
+		if (!p.node)
+			return {};
+		if (p.type.func && p.type.ptr == 1)
+			return p;
+		if (!isPointer(p.type)) {
+			curOffset = e->offset;
+			fail("indirection requires a pointer operand");
+			return {};
+		}
+		CType pt = pointee(p.type);
+		if (isArrayType(pt))
+			return {p.node, decay(pt)};
+		if (isAggregate(pt))
+			return {p.node, pt};
+		return {fn.load(irType(pt), p.node), pt};
+	}
+
+	Emitter::Value Emitter::emitUnary(Function& fn, const Expr* e) {
+		switch (e->unary.op) {
+		case ExprOp::PreInc:
+		case ExprOp::PreDec:
+		case ExprOp::PostInc:
+		case ExprOp::PostDec:
+			return emitIncDec(fn, e);
+		case ExprOp::Addr:
+			return emitAddrOf(fn, e);
+		case ExprOp::Deref:
+			return emitDeref(fn, e);
+		default:
+			break;
+		}
+		Value v = emitExpr(fn, e->unary.operand);
+		if (!v.node)
+			return {};
+		if (e->unary.op == ExprOp::Real) {
+			if (isComplexType(v.type))
+				return {complexReal(fn, v), complexElem(v.type)};
+			return v;
+		}
+		if (e->unary.op == ExprOp::Imag) {
+			if (isComplexType(v.type))
 			break;
 		case ExprOp::AddAssign:
 			result = fn.add(fn.get(var), rhs);
