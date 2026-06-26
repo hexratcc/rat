20e9a788fe731e313aa2d13c4c5f894d85d36769
nella <simontupy64@gmail.com>
2026-06-26 16:09:44 +0200

    Implement machine pass.

--- include/Pass/Pass.h
+++ include/Pass/Pass.h
@@ -5,7 +5,10 @@
 
 namespace rat {
 	struct Function;
+	struct MachineFunc;
+	struct MachineModule;
 	struct Module;
+	struct TargetInfo;
 
 	struct Pass {
 		virtual ~Pass();
@@ -17,6 +20,19 @@ namespace rat {
 	struct FunctionPass : Pass {
 		B32 run(Module& module) final;
 		virtual U32 runOnFunction(Function& fn) = 0;
+	};
+
+	struct MachinePass {
+		virtual ~MachinePass();
+
+		virtual const C8* name() const = 0;
+		virtual B32 run(Module& module, MachineModule& mm, const TargetInfo& target) = 0;
+	};
+
+	struct MachineFunctionPass : MachinePass {
+		B32 run(Module& module, MachineModule& mm, const TargetInfo& target) final;
+		virtual U32
+		runOnMachineFunction(const Function& fn, MachineFunc& mf, const TargetInfo& target) = 0;
 	};
 } // namespace rat
 
--- lib/Pass/Pass.cpp
+++ lib/Pass/Pass.cpp
@@ -1,14 +1,23 @@
 #include "Pass/Pass.h"
 
+#include "CodeGen/MachineModule.h"
 #include "IR/Module.h"
 
 namespace rat {
 	Pass::~Pass() = default;
+	MachinePass::~MachinePass() = default;
 
 	B32 FunctionPass::run(Module& module) {
 		U32 changed = 0;
 		for(Function* fn : module)
 			changed += runOnFunction(*fn);
+		return changed != 0;
+	}
+
+	B32 MachineFunctionPass::run(Module& module, MachineModule& mm, const TargetInfo& target) {
+		U32 changed = 0;
+		for(Function* fn : module)
+			changed += runOnMachineFunction(*fn, mm.get(fn), target);
 		return changed != 0;
 	}
 } // namespace rat
