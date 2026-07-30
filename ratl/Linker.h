d2f861907c108da240ccbf7704ea5f10ab5c71ba
nella <simontupy64@gmail.com>
2026-07-30 10:31:37 +0200

    Linker api.

---
+++ ratl/Linker.h
@@ -0,0 +1,27 @@
+#ifndef RAT_RATL_LINKER_H
+#define RAT_RATL_LINKER_H
+
+#include "Core.h"
+
+namespace rat {
+	enum class LinkTarget {
+		LinuxX64,
+		WindowsX64 // TODO
+	};
+
+	struct LinkOptions {
+		List<String> inputs;
+		String output;
+		List<String> libPaths; // -L dirs
+		List<String> libs;		 // -l names
+		String interp;
+		List<String> rpaths;	 // DT_RUNPATH dirs
+		String entry = "main";
+		B32 executable = true;
+		LinkTarget target = LinkTarget::LinuxX64;
+	};
+
+	B32 link(const LinkOptions& opt, String& err);
+} // namespace rat
+
+#endif
